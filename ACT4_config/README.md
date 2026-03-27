34cd9b58598338becd1844902fd2f312e8c6c11e
Zain2050 <zainzahid2050@gmail.com>
2026-03-27 12:00:13 +0500

    ACT4 config files for Buggy-V

---
+++ ACT4_config/README.md
@@ -0,0 +1,8 @@
+## Setup Instructions
+
+1. Clone **riscv-arch-test (ACT4)**:
+
+   ```bash
+   git clone https://github.com/riscv/riscv-arch-test
+
+2. Copy `./buggy-v` folder into `riscv-arch-test/config/cores`
---
+++ ACT4_config/buggy-v/buggy-v-rv32/rvtest_config.svh
@@ -0,0 +1,14 @@
+`define XLEN32
+`define RAM_BASE_ADDR 32'h8000_0000
+`define ACCESS_FAULT_ADDRESS 64'h0000_0000
+`define CLINT_BASE 64'h0200_0000
+
+`define HAS_I
+`define HAS_M
+`define HAS_A
+`define HAS_C
+`define HAS_ZICSR
+`define HAS_ZIFENCEI
+`define HAS_S
+`define HAS_U
+`define HAS_SV32
